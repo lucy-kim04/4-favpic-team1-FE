0659cfbc525b79d4fec6f1a80e341a82605be2e3
Hyeongmin Jo <paengdal1@gmail.com>
2025-02-15 20:31:55 +0900

    feat: Auth Provider구현, axios interceptor 구현, 기타 - Auth Provider   - AuthContext.jsx(/contexts) - axios interceptor   - /api/client.js 파일 내 - 기타   - 라우팅 관련 파일 중 js 확장자인 파일명을 jsx로 변경   - HomePage는 충돌이 우려되어 추후 변경 예정

--- api/client.js
+++ api/client.js
@@ -1,10 +1,67 @@
 import axios from 'axios';
+import usersApi from './users/users.api';
 
 const baseURL = 'http://localhost:5050';
 
 export const client = axios.create({
   baseURL,
 });
+
+// ---------------- axios interceptors 설정하기
+// request interceptor
+// - headers에 accessToken 실어 보내기
+client.interceptors.request.use(
+  (config) => {
+    if (
+      config.url === '/users/refresh-token' ||
+      config.url === '/users/sign-up' ||
+      config.url === '/users/log-in' ||
+      config.url === '/users/check-nickname'
+    )
+      return config;
+    let accessToken;
+    if (typeof window !== 'undefined') {
+      accessToken = localStorage.getItem('accessToken');
+    }
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// response interceptor
+// - 401 에러 시 refreshToken 요청(accessToken 재발급) 후 -에러가 발생한- 기존 요청을 재요청
+client.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    const statusCode = error.response?.status;
+    if ((statusCode === 401 || statusCode === 419) && !originalRequest._retry) {
+      console.log('토큰 만료');
+      originalRequest._retry = true;
+      let prevRefreshToken;
+      if (typeof window !== 'undefined') {
+        prevRefreshToken = localStorage.getItem('refreshToken');
+      }
+      if (!prevRefreshToken) {
+        return;
+      }
+      const { accessToken } = await usersApi.refreshToken(prevRefreshToken);
+      if (typeof window !== 'undefined') {
+        localStorage.setItem('accessToken', accessToken);
+      }
+      originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+      return client.request(originalRequest);
+    }
+    return Promise.reject(error);
+  }
+);
 
 export function errorHandler(error) {
   console.log('AxiosError', error);
