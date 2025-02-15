9ddfe092378fad707f9d128fa4c1ea2ce1532449
Hyeongmin Jo <paengdal1@gmail.com>
2025-02-15 17:54:18 +0900

    feat: 엔드포인트에 대응하는 userApi 함수들 구현 - signUp, logIn, refreshToken, isAvailableNickname, getMe - 공통적으로 필요한 axios  errorHandler 작성(in client.js)

--- api/client.js
+++ api/client.js
@@ -5,3 +5,12 @@ const baseURL = 'http://localhost:5050';
 export const client = axios.create({
   baseURL,
 });
+
+export function errorHandler(error) {
+  console.log('AxiosError', error);
+  if (error.response) {
+    throw new Error(`${error.response.status}: ${error.response.data}`);
+  } else {
+    throw new Error(error, '요청에 실패하였습니다.');
+  }
+}
