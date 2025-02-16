263d6a5f500275522502536b4da1ae563fb0951a
Hyeongmin Jo <paengdal1@gmail.com>
2025-02-16 09:32:45 +0900

    임시커밋

--- app/(providers)/(root)/auth/log-in/page.jsx
+++ app/(providers)/(root)/auth/log-in/page.jsx
@@ -1,5 +1,23 @@
+import logo from '@/assets/images/logo.png';
+import Image from 'next/image';
+
 function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div className="h-[100vh] flex justify-center items-center bg-[#0f0f0f]">
+      <div className="w-[520px]">
+        <Image src={logo} alt="로고" className="w-[330px]" />
+        <form className="w-full">
+          <div className="inline-flex flex-col w-full">
+            <label>이메일</label>
+            <input type="email" id="email" />
+            <label>비밀번호</label>
+            <input type="password" id="password" />
+            <button>로그인</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPage;
