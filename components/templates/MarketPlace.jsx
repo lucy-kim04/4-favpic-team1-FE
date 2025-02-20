4ae1f23cd0cb614ed61f4d06b72eeb7ac435c8c1
HJ <a01025105259@gmail.com>
2025-02-20 13:41:28 +0900

    main화면 dropdown component prop 수정, component 수정

--- components/templates/MarketPlace.jsx
+++ components/templates/MarketPlace.jsx
@@ -1,27 +1,27 @@
-'use client';
+"use client";
 
-import shopsApi from '@/api/shops/shops.api';
-import constants from '@/constant';
-import { useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import Dropdown from '../atoms/Dropdown';
-import InputSearch from '../molecules/InputSearch';
-import Title from '../molecules/Title';
-import CardList from '../organisms/CardList';
+import shopsApi from "@/api/shops/shops.api";
+import constants from "@/constant";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import Dropdown from "../atoms/Dropdown";
+import InputSearch from "../molecules/InputSearch";
+import Title from "../molecules/Title";
+import CardList from "../organisms/CardList";
 
 function MarketPlace({ initialData }) {
-  const [orderBy, setOrderBy] = useState('최신 순');
-  const [grade, setGrade] = useState('등급');
-  const [genre, setGenre] = useState('장르');
-  const [onSale, setOnSale] = useState('매진 여부');
-  const [keyword, setKeyword] = useState('');
+  const [orderBy, setOrderBy] = useState("최신 순");
+  const [grade, setGrade] = useState("등급");
+  const [genre, setGenre] = useState("장르");
+  const [onSale, setOnSale] = useState("매진 여부");
+  const [keyword, setKeyword] = useState("");
 
-  const { handleSubmit, control } = useForm({ defaultValues: { search: '' } });
+  const { handleSubmit, control } = useForm({ defaultValues: { search: "" } });
 
   const searchOptions = { orderBy, grade, genre, onSale, keyword };
   const { data: shops, isPending } = useQuery({
-    queryKey: ['shops', { ...searchOptions }],
+    queryKey: ["shops", { ...searchOptions }],
     queryFn: () => shopsApi.getShops(searchOptions),
     initialData,
     staleTime: 0,
@@ -42,7 +42,7 @@ function MarketPlace({ initialData }) {
         <Title
           intent="xl"
           onClick={() => {
-            alert('구현중');
+            alert("구현중");
           }}
           className="sm:hidden"
         >
@@ -52,23 +52,26 @@ function MarketPlace({ initialData }) {
           <form onSubmit={handleSubmit(handleSubmitSearch)}>
             <InputSearch
               control={control}
-              name={'search'}
-              placeholder={'검색'}
+              name={"search"}
+              placeholder={"검색"}
               size="md"
             />
           </form>
           <div className="flex gap-11 shrink-0 ml-[60px]">
             <Dropdown
+              width="w-[134px]"
               label={grade}
               options={constants.CARD_GRADES}
               onSelect={setGrade}
             />
             <Dropdown
+              width="w-[134px]"
               label={genre}
               options={constants.CARD_GENRES}
               onSelect={setGenre}
             />
             <Dropdown
+              width="w-[140px]"
               label={onSale}
               options={constants.CARD_ON_SALE}
               onSelect={setOnSale}
@@ -77,6 +80,7 @@ function MarketPlace({ initialData }) {
           <div className="w-full grow-1"></div>
           <div className="shrink-0">
             <Dropdown
+              width="w-[180px]"
               label={orderBy}
               options={constants.SORT_OPTIONS}
               isBox={true}
