9cafebf4b02c181d02e564c3908f4727b2c2fbae
Hyeongmin Jo <paengdal1@gmail.com>
2025-02-24 09:12:17 +0900

    hotfix: ModalBackGround.jsx 파일명앞에 공백 제거 후 긴급 푸시

--- components/organisms/CardList.jsx
+++ components/organisms/CardList.jsx
@@ -1,5 +1,14 @@
 import Card from './Card';
 
+/**
+ * 카드 컴포넌트 옵션
+ * - card: card 데이터
+ * - intent: 카드의 형태 구분
+ *   - shop(기본값): 마켓 플레이스 목록
+ *   - gallery: /my-cards/gallery의 목록
+ *   - sales: /my-cards/sales의 목록
+ *   - exchange: 판매포토 카드 상세(판매자) 페이지의 하단 '교환 제시 목록'
+ */
 function CardList({ cards, intent }) {
   return (
     <div className="flex justify-center">
