0bf34f819607c699d56781f8287fab001f3964ce
Hyeongmin Jo <paengdal1@gmail.com>
2025-03-01 22:24:52 +0900

    feat: /api/notifications/notifications.api.js 추가 - sendNotification, getNotificationsOfMe 추가

---
+++ api/notifications/notifications.api.js
@@ -0,0 +1,30 @@
+import { client, errorHandler } from '../client';
+
+const sendNotification = async (dto) => {
+  try {
+    const url = '/notifications';
+    const response = await client.post(url, dto);
+
+    return response.data;
+  } catch (error) {
+    errorHandler(error);
+  }
+};
+
+const getNotificationsOfMe = async () => {
+  try {
+    const url = '/notifications/me';
+    const response = await client.get(url);
+
+    return response.data;
+  } catch (error) {
+    errorHandler(error);
+  }
+};
+
+const notificationsApi = {
+  sendNotification,
+  getNotificationsOfMe,
+};
+
+export default notificationsApi;
