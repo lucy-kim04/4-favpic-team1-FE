d336abeddc58aadddbbbd6a5a4e5098fc00b61d1
paengdal <40021741+paengdal@users.noreply.github.com>
2025-03-06 10:20:05 +0900

    [조형민] 로그인/회원가입 버튼에 Loader추가, axios baseUrl을 render 배포 주소로 변경 Feature/111 (#120)
    * fix: 알림 팝업 레이어 toggle 오류 해결
    
    * refactor: 로그아웃된 상태에서 로그인/회원가입/마켓플레이스 외의 url에 있을 경우 마켓플레이스로 이동
    
    * chore: 주석, 파일 정리
    
    * feat: pull 후 Gnb 충돌 해결, 알림 레이어 toggel 문제 해결
    
    * fix: Detail.jsx 쿼리키 수정, intent명 market-> shop
    
    * fix: Gnb 알림, 메뉴 레이어 팝업 수정
    - 메뉴 레이어 toggle 문제 수정
    - 한쪽이 열려있을 때 다른 쪽을 열면 열려있던 레이어를 닫고 새로운 레이어만 열기
    
    * feat: getMe의 staleTime을 모두 0으로 설정
    - 테스트 시 로그인/로그아웃 빈도가 잦아 적용
    
    * feat&fix: 로그인/회원가입 버튼에 Loader추가, axios baseUrl을 render 배포 주소로 변경

--- api/client.js
+++ api/client.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import usersApi from './users/users.api';
 
-const baseURL = 'http://localhost:5050';
+// const baseURL = 'http://localhost:5050';
+const baseURL = 'https://four-favpic-team1-be.onrender.com/';
 
 export const client = axios.create({
   baseURL,
