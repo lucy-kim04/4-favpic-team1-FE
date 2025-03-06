77947cd1eb1fd56cec10c1c065261c291e9c8a9a
HJ <a01025105259@gmail.com>
2025-03-06 10:30:16 +0900

    hotfix:suspensed로 변경

--- app/(providers)/layout.jsx
+++ app/(providers)/layout.jsx
@@ -1,17 +1,14 @@
 import { AuthProvider } from '@/contexts/AuthContext';
 import { ModalProvider } from '@/contexts/ModalContext';
 import TanstackQueryProvider from '@/libs/tanstack-query';
-import { Suspense } from 'react';
 
 function ProvidersLayout({ children }) {
   return (
-    <Suspense>
-      <TanstackQueryProvider>
-        <AuthProvider>
-          <ModalProvider>{children}</ModalProvider>
-        </AuthProvider>
-      </TanstackQueryProvider>
-    </Suspense>
+    <TanstackQueryProvider>
+      <AuthProvider>
+        <ModalProvider>{children}</ModalProvider>
+      </AuthProvider>
+    </TanstackQueryProvider>
   );
 }
 
